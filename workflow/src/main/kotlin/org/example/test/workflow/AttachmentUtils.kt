1b1856907f913a9bdd94c5e29686a589050334c8
Artem Vlasov <artemv@tradeix.com>
2021-11-04 11:45:07 +0200

    Add erase

--- workflow/src/main/kotlin/org/example/test/workflow/AttachmentUtils.kt
+++ workflow/src/main/kotlin/org/example/test/workflow/AttachmentUtils.kt
@@ -14,18 +14,27 @@ object AttachmentUtils {
 
     private const val filename = "Large-Sample-test1.png"
     private val largeFile = File("../../../../Large-Sample-test1.png")
-    private val contentByteArray = largeFile.readBytes()
 
-    private fun zipContent(): ByteArrayInputStream =
-        ByteArrayOutputStream(contentByteArray.size).let {
-            ZipOutputStream(it).use { zos ->
-                val entry = ZipEntry(prepareZipArchiveFileName())
-                zos.putNextEntry(entry)
-                zos.write(contentByteArray)
+    private fun zipContent(): ByteArrayInputStream {
+        var fileContent: ByteArray? = largeFile.readBytes()
+        var zipContent: ByteArray? = null
+        try {
+            ByteArrayOutputStream(contentByteArray.size).let {
+                ZipOutputStream(it).use { zos ->
+                    val entry = ZipEntry(prepareZipArchiveFileName())
+                    zos.putNextEntry(entry)
+                    zos.write(contentByteArray)
+                }
+
+                zipContent = it.toByteArray()
+
+                zipContent!!.inputStream()
             }
-
-            it.toByteArray().inputStream()
+        } finally {
+            fileContent = null
+            zipContent = null
         }
+    }
 
     private fun prepareZipArchiveFileName() =
         "${FilenameUtils.getName(filename)}-${UUID.randomUUID()}.${FilenameUtils.getExtension(filename)}"
